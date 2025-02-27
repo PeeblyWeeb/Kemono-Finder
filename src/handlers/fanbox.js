e6ec8ecec4c7d514cea514eb6fc115c5e7e4d3c1
peeblyweeb <pebblykat@gmail.com>
2025-02-27 07:06:35 -0500

    Implement fanbox scraping

--- src/handlers/fanbox.js
+++ src/handlers/fanbox.js
@@ -1,1 +1,17 @@
-console.log("Hello, world!");
+const usernames = [
+  /https:\/\/(.*)\.fanbox\.cc/gm.exec(document.location.href), // username.fanbox.cc
+  /https:\/\/fanbox\.cc\/@(.*)/gm.exec(document.location.href), // fanbox.cc/@username
+  /(.*)｜pixivFANBOX/gm.exec(document.title), // actual display name
+]
+  .filter((match) => match)
+  .map((match) => match[1]);
+
+usernames.forEach((username) => {
+  fetch("https://corsproxy.io/?url=https://kemono.su/api/v1/creators.txt")
+    .then((response) => response.json())
+    .then((json) => json.filter((creator) => creator.name === username))
+    .then((filtered) =>
+      filtered.forEach((creator) => console.log(`https://kemono.su/${creator.service}/user/${creator.id}`))
+    );
+});
+console.log("Hello, world!", usernames);
