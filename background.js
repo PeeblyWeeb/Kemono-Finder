4add4884f1a4eeadd53b412c25fccea0d8a3da2c
peeblyweeb <peebly@salamithecat.com>
2025-08-18 14:03:08 -0400

    fix api requests because kemono made undocumented changes

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 const PROXY = "https://corsproxy.io/?url=";
-const KEMONOROOT = "https://kemono.cr"
+const KEMONOROOT = "https://kemono.cr";
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type == "postThumbnailURLs") {
@@ -29,10 +29,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         func: () => {
           chrome.runtime.sendMessage({
             type: "postThumbnailURLs",
-            urls: Array.from(document.querySelector(".post__files").children).map(
-              (postThumbnail) => postThumbnail.children[0].children[0].href
+            urls: Array.from(
+              document.querySelector(".post__files").children,
+            ).map(
+              (postThumbnail) => postThumbnail.children[0].children[0].href,
             ),
-            postId: /https:\/\/kemono\.cr\/[A-z]+\/user\/[0-9]+\/post\/([0-9]+)/gm.exec(document.location.href)[1],
+            postId:
+              /https:\/\/kemono\.cr\/[A-z]+\/user\/[0-9]+\/post\/([0-9]+)/gm.exec(
+                document.location.href,
+              )[1],
           });
         },
       });
@@ -42,15 +47,24 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     chrome.tabs.query({ active: true, currentWindow: true }, async ([tab]) => {
       chrome.tabs.sendMessage(tab.id, { type: "getResults" }, async (res) => {
         // find matching users
-        const response = await fetch(PROXY + KEMONOROOT + "/api/v1/creators.txt");
+        const response = await fetch(
+          PROXY + KEMONOROOT + "/api/v1/creators",
+          {
+            headers: {
+              Accept: "text/css",
+            },
+          },
+        );
         const creators = await response.json();
+        console.log(creators);
 
         console.log(res);
         const creator_matches = res.searchValues.flatMap((value) =>
-          creators.filter((creator) => creator[res.searchKey] == value)
+          creators.filter((creator) => creator[res.searchKey] == value),
         );
         const unique_creator_matches = creator_matches.filter(
-          (item, index, self) => index === self.findIndex((t) => t.id === item.id)
+          (item, index, self) =>
+            index === self.findIndex((t) => t.id === item.id),
         );
 
         sendResponse({
@@ -62,7 +76,14 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
   if (message.type == "getPost") {
     fetch(
-      PROXY + KEMONOROOT + `/api/v1/${message.creator.service}/user/${message.creator.id}/post/${message.postId}`
+      PROXY +
+      KEMONOROOT +
+      `/api/v1/${message.creator.service}/user/${message.creator.id}/post/${message.postId}`,
+      {
+        headers: {
+          Accept: "text/css",
+        },
+      },
     )
       .then((res) => res.json())
       .then(sendResponse)
